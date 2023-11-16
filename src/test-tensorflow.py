134cd85ae841af08905e04901f3df39496e63f6f
Zianor <zianor@zianor.de>
2023-11-16 14:38:31 +0100

    Setup python project and test tensorflow

---
+++ src/test-tensorflow.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# Check for TensorFlow GPU access
+print(tf.config.list_physical_devices())
+
+# See TensorFlow version
+print(tf.__version__)
+
+with tf.device("/GPU"):
+    a = tf.random.normal(shape=(2,), dtype=tf.float32)
+    b = tf.nn.relu(a)
+
+print(a)
+print(b)
