d616b898fd67871f2cfc1d06083ad94831109d81
Zianor <zianor@zianor.de>
2023-12-16 12:16:07 +0100

    Improve plotting in notebook and add global models

--- src/helper.py
+++ src/helper.py
@@ -1,5 +1,8 @@
 import os
+from math import ceil
 from pathlib import Path
+
+from matplotlib import pyplot as plt
 
 
 def get_data_dirs(colab=False):
@@ -18,3 +21,26 @@ def get_checkpoint_path(model_name, colab=False):
         return f"/content/drive/MyDrive/DLIV/wacv/{model_name}.h5"
     else:
         return os.path.join(Path(__file__).parent, "wacv", f"{model_name}.h5")
+
+
+def plot_first_batch(model, test_gen, two_inputs=False, batch_size=16):
+    for image, label in test_gen:
+        res = model.predict(image)
+        num_rows = ceil(batch_size / 2)
+        plt.subplots(num_rows, 6, figsize=(12, 16))
+        for i in range(batch_size):
+            plt.subplot(num_rows, 6, i * 3 + 1)
+            if two_inputs:
+                plt.imshow(image[0][i, :, :, :])
+            else:
+                plt.imshow(image[i, :, :, :])
+            plt.axis("off")
+            plt.subplot(num_rows, 6, i * 3 + 2)
+            plt.imshow(res[i, :, :, :], cmap="gray")
+            plt.axis("off")
+            plt.subplot(num_rows, 6, i * 3 + 3)
+            plt.imshow(label[i, :, :, :], cmap="gray")
+            plt.axis("off")
+        plt.tight_layout()
+        plt.show()
+        break
