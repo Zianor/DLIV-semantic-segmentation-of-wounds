387f76386ce7e16d0be4f9c243996298f5113824
Zianor <zianor@zianor.de>
2023-12-16 12:55:55 +0100

    Create markdown table for every possible model in model evaluation

--- src/WSNET/evaluate_all_models.py
+++ src/WSNET/evaluate_all_models.py
@@ -5,19 +5,58 @@ os.environ["SM_FRAMEWORK"] = "tf.keras"
 
 from src.WSNET.wsnet import train_model
 
+
+def get_architecture_title(model_architecture):
+    architectures = {"local": "Local model", "global": "Global model", "global-local": "Global-Local model"}
+    return architectures[model_architecture]
+
+
+def get_segmentation_model_title(model):
+    models = {"unet": "U-Net", "linknet": "Linknet", "pspnet": "PSPNet", "fpn": "FPN"}
+    return models[model]
+
+
+def create_markdown_table_str_for_metrics(results_dict):
+    markdown_lines = []
+    table_keys = "| " + " | ".join(f"{key:<5}" for key in results_dict.keys()) + " |"
+    markdown_lines.append(table_keys)
+    markdown_lines.append("|" + "|".join("-" * max(len(key) + 2, 7) for key in results_dict.keys()) + "|")
+    table_values = (
+        "| "
+        + " | ".join(
+            f"{value:.3f}{' ' * (len(key) - 5) if len(key) > 5 else ''}"
+            for key, value in results_dict.items()
+        )
+        + " |"
+    )
+    markdown_lines.append(table_values)
+    markdown_lines.append("")
+    return "\n".join(markdown_lines)
+
+
 if __name__ == "__main__":
     test_images_count = 403
     batch_size = 16
-    segmentation_models = ["unet", "pspnet", "fpn", "linknet"]
-    model_architectures = ["global-local", "local", "global"]
+    segmentation_models = ["unet", "linknet", "pspnet", "fpn"]
+    model_architectures = ["local", "global", "global-local"]
+    markdown_lines = []
     for model_architecture in model_architectures:
+        markdown_lines.append(f"# {get_architecture_title(model_architecture)}")
+        markdown_lines.append("")
         for segmentation_model in segmentation_models:
+            markdown_lines.append(f"### {get_segmentation_model_title(segmentation_model)}")
+            markdown_lines.append("")
             model, train_gen, val_gen, test_gen = train_model(
                 segmentation_model=segmentation_model, model_architecture=model_architecture, load_only=True
             )
             results = model.evaluate(test_gen, steps=ceil(float(test_images_count) / float(batch_size)))
             results_dict = dict(zip(model.metrics_names, results))
+            markdown_lines.append(create_markdown_table_str_for_metrics(results_dict))
             print(f"Model {segmentation_model} {model_architecture}")
             print(", ".join(f"{key}: {value:.3f}" for key, value in results_dict.items()))
-            print("--")
         print("-----")
+
+    with open("evaluation_results.md", "w") as writer:
+        for line in markdown_lines:
+            writer.write(line)
+            writer.write("\n")
